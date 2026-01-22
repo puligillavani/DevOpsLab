89249c8a6c33e96101097085ffe8533f81c3fed0
Vani Puligilla <puligilla.vani@gmail.com>
2026-01-22 14:22:23 +0530

    code added by Dev2

--- app.java
+++ app.java
@@ -10,3 +10,10 @@ public class CheckEvenOdd {
            System.out.println(num + " is odd");
    }
 }
+
+//code added by Developer-2 
+public class Main {
+   public static void main(String[] args) {
+       System.out.println("Hello World");
+   }
+}
