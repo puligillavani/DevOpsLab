d792e91703482a69dd58aa9f6a38b07cd906bf81
EC2 Default User <ec2-user@ip-172-31-70-245.ec2.internal>
2026-01-22 09:06:23 +0000

    now dev 1 adding data

--- app.java
+++ app.java
@@ -15,5 +15,14 @@ public class CheckEvenOdd {
 public class Main {
    public static void main(String[] args) {
        System.out.println("Hello World");
+
+//code added by Developer 1
+public class AddTwoIntegers {
+   public static void main(String[] args) {
+       int first = 10;
+       int second = 20;
+       int sum = first + second;
+       System.out.println("The sum is: " + sum);
+>>>>>>> Stashed changes
    }
 }
